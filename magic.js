a2bbf167741e8ee3dcd8b628719ce9f7d4d604c0
rick <vrick6749@gmail.com>
2021-08-29 15:31:23 +1000

    added ternary with name variable

--- magic.js
+++ magic.js
@@ -1,1 +1,2 @@
-let userName = "Jack"//Can be any name
+let userName = ""//Can be any name
+userName ? console.log(`Hello, ${userName}`): console.log("Hello") // checks if userName is true in this case truthy or falsey and runs console.log respectively
