cd08d68dd608cfac5342592206c5f9ce5ac36ad4
rick <vrick6749@gmail.com>
2021-08-29 15:34:32 +1000

    added userquestion variable and logging it out to console

--- magic.js
+++ magic.js
@@ -1,2 +1,9 @@
 let userName = ""//Can be any name
-userName ? console.log(`Hello, ${userName}`): console.log("Hello") // checks if userName is true in this case truthy or falsey and runs console.log respectively
+
+
+userName ? console.log(`Hello, ${userName}`): console.log("Hello") // checks if userName is true in this case truthy or falsey and runs the code respectively
+
+
+let userQuestion = "How old are you" // can be any question the user wants to place in
+
+console.log(userQuestion) // just logs the question typed in by user
